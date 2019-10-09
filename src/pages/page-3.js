0a5f4afb81ce018e2f73f1d1e410ad6d6850cc4e
Courtney Corlew <courtneyecorlew@gmail.com>
2019-10-09 19:33:53 -0400

    added third page

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -4,15 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const ThirdPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/page-3/">Go to page 3</Link>
-
+    <SEO title="Page three" />
+    <h1>Hi from the three page</h1>
+    <p>Welcome to page 3</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default ThirdPage
